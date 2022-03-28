92573fab1add5e684b13ea8fdfbd8cda7007a3cd
rakesh <rakeshbishnoi4577@gmail.com>
2022-03-28 20:46:32 +0530

    router fix

--- src/index.js
+++ src/index.js
@@ -4,20 +4,22 @@ import "./index.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Home from "./home/view";
+import Services from "./services/view";
 
 import ThemeConfig from "./theme";
 import GlobalStyles from "./theme/globalStyles";
-import services from "./services/view";
 import { Routes } from "react-router-dom";
 
 function App() {
   return (
+    // <div>hello</div>
+    
     <ThemeConfig>
       <GlobalStyles />
       <Router>
         <Routes>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/services" component={services} />
+          <Route path="/" element={<Home />} />
+          <Route path="Services" element={<Services />} />
         </Routes>
       </Router>
     </ThemeConfig>
