f5f23369acff36c134ab4b5ab0e66b9cc8b433bc
rakesh <rakeshbishnoi4577@gmail.com>
2022-04-12 11:20:26 +0530

    serv

--- src/components/heading.js
+++ src/components/heading.js
@@ -1,15 +1,29 @@
+import { Breadcrumbs, Link } from "@mui/material";
 import styles from "./css/heading.module.css";
 
 export default function Heading() {
   return (
-    <div
-      style={{
-        backgroundImage: "url(images/title-bg.jpg)",
-      }}
-      className={styles.main}
-    >
-        
-    </div>
+    <>
+      <div
+        style={{
+          backgroundImage: "url(images/title-bg.jpg)",
+        }}
+        className={styles.main}
+      >
+        <div className={styles.bg}>
+          <h1>Services</h1>
+        </div>
+      </div>
+      <div className={styles.breadcrumbs}>
+        <div className={styles.breadcrumbs2} >   
+          <Breadcrumbs aria-label="breadcrumb">
+            <Link underline="hover" color="inherit" href="/">
+              Home
+            </Link>
+            <p  >Services</p>
+          </Breadcrumbs>
+        </div>
+      </div>
+    </>
   );
 }
-          
