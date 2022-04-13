44143731ada90c7c81c1aadc5ca834e07066e6b3
rakesh <rakeshbishnoi4577@gmail.com>
2022-04-13 22:37:19 +0530

    idk :(

--- src/pages/services/view.js
+++ src/pages/services/view.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+
 import Heading from "../../components/heading";
 import { NavBar } from "../home/components/header";
+import styles from "./css/style.module.css";
 
-export default function services() {
+export default function Services() {
+  const [categories, setcategories] = useState([]);
+
+  const [services, setServices] = useState([]);
+
+  const [selectedServices, setSelectedServices] = useState([]);
+
+
+
   return (
     <div>
       <NavBar />
-      <Heading/>
-
-
+      <Heading />
+      <div className={styles.main}>
+        <div className={styles.sections}>
+          <div className={`${styles.categories} ${styles.section}`}>
+            <h3>categories</h3>
+            <span className={styles.Divider} />
+          </div>
+          <div className={`${styles.services} ${styles.section}`}>
+            <h3>Services</h3>
+            <span className={styles.Divider} />
+          </div>
+          <div className={`${styles.selected_services} ${styles.section}`}>
+            <h3>Selected Services</h3>
+            <span className={styles.Divider} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-
