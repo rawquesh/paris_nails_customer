6cb9dde0eb8150cba52e12e389157249e03e4ae6
rakesh <rakeshbishnoi4577@gmail.com>
2022-04-25 23:36:58 +0530

    donme

--- src/pages/staff_time/view.js
+++ src/pages/staff_time/view.js
@@ -1,11 +1,6 @@
-import {
-  collection,
-  documentId,
-  getDocs,
-  query,
-  where,
-} from "firebase/firestore";
 import React, { useEffect, useState } from "react";
+
+import { collection, documentId, getDocs, where } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import Heading from "../../components/heading";
 import { db } from "../../utils/firebaseConfig";
@@ -15,11 +10,25 @@ import useQuery from "../../utils/functions/use_query";
 import Footer, { Bottom } from "../home/components/footer";
 import { NavBar } from "../home/components/header";
 
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { TextField } from "@mui/material";
+import { addDays, isAfter, isBefore, isDate, subDays } from "date-fns";
+
+import styles from "./style.module.css";
+import {
+  DatePicker,
+  DesktopDatePicker,
+  MobileDatePicker,
+} from "@mui/x-date-pickers";
+
 export default function ChooseStaffTime() {
   const navigate = useNavigate();
   const query = useQuery();
 
   const [services, setServices] = useState([]);
+  const [date, setDate] = React.useState(new Date());
+  const [datePickerError, setDatePickerError] = React.useState(null);
 
   useEffect(() => {
     // fetchServices();
@@ -44,13 +53,86 @@ export default function ChooseStaffTime() {
     }
   }
 
+  function handleChange(newValue) {
+    setDate(newValue);
+
+    // console.log(newValue);
+
+
+    if (isDate(newValue)) {
+      let now = new Date();
+      let later = addDays(now, 16);
+      let newDate = newValue;
+
+      newDate.setHours(23, 59, 59, 59);
+
+      const validate = isAfter(newDate, now) && isBefore(newDate, later);
+
+      if (validate) {
+        console.log("correct date is ", newDate);
+      }
+    }
+  }
+
   return (
     <div>
       <NavBar />
       <Heading title="Choose a day & time" />
-      
+      <div className={styles.main}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "flex-start",
+            flexWrap: "wrap",
+          }}
+        >
+          <BuildTitle title="Choose a day" />
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DatePicker
+              minDate={new Date()}
+              maxDate={addDays(new Date(), 15)}
+              label="Pick a date"
+              inputFormat="MM/dd/yyyy"
+              value={date}
+              onChange={handleChange}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  helperText={datePickerError}
+                  error={datePickerError !== null}
+                  size="small"
+                />
+              )}
+              onError={(r, v) => {
+                let error = null;
+                if (r === "maxDate") {
+                  error = "Date should be under 15 days from now";
+                } else if (r === "minDate") {
+                  error = "You can not pick older dates.";
+                } else if (r === "invalidDate") {
+                  error = "Pick a vaild date.";
+                } else if (typeof r === String) {
+                  error = r;
+                }
+                setDatePickerError(error);
+              }}
+              pro
+            />
+          </LocalizationProvider>
+        </div>
+      </div>
+
       <Footer />
       <Bottom />
+    </div>
+  );
+}
+
+function BuildTitle({ title, widget }) {
+  return (
+    <div style={{ marginRight: "10px", marginBottom: "10px" }}>
+      <h3 className={styles.midHeading}>{title}</h3>
+      {widget}
     </div>
   );
 }
